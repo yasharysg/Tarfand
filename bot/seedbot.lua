97a437de9efb7caee2dec35bef17dcf8b9567b40
yashartg <21157249+yasharsa@users.noreply.github.com>
2017-09-02 20:24:33 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -262,7 +262,7 @@ function create_config( )
     "help"
 
     },
-    sudo_users = {125734499,0,0,0,tonumber(our_id)},--Sudo users
+    sudo_users = {391372093,0,0,0,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v4
 An advanced administration bot based on TG-CLI written in Lua
